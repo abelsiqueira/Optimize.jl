cc51d9c25c4b3eeaaa04686beefa11b10090a14a
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-10-14 18:56:47 -0300

    Implement blackbox solver with MADS

---
+++ src/tuning/mads.jl
@@ -0,0 +1,61 @@
+using Primes
+
+# t = ∑ᵣ aᵣ pʳ
+function expansion(t :: Int, p :: Int)
+  p > 1 || error("base $p not supported")
+  t == 0 && return [0]
+  E = Int[]
+  while t > 0
+    push!(E, t % p)
+    t = div(t, p)
+  end
+  return E
+end
+
+function halton_utp(t :: Int, p :: Int)
+  E = expansion(t, p)
+  ne = length(E)
+  return sum(E[i] / p^i for i = 1:ne)
+end
+
+# uₜ = (uₜₚ₁,…,uₜₚₙ)
+# Starts at m-th prime
+function halton(t :: Int, n :: Int)
+  # Get n primes
+  return [halton_utp(t, Primes.PRIMES[i]) for i = 1:n]
+end
+
+# qₜℓ
+function adjhalton(t :: Int, n :: Int, ℓ :: Int)
+  ut = halton(t, n)
+  q = 2ut - 1
+  #α = sqrt(2^abs(ℓ)/n) - 0.5
+  j = -1
+  α = 0.0
+  qt = q
+  done = false
+  isol = 0
+  jsol = 0
+  while !done
+    j += 1
+    done = true
+    for i = 1:n
+      ξ = (j + 0.5) * norm(q) / abs(q[i])
+      qt = round.(Int, ξ * q / norm(q), RoundNearestTiesAway)
+      qt[i] = (j + 1) * sign(q[i])
+      if dot(qt, qt) <= 2^abs(ℓ)
+        if α < ξ
+          α = ξ
+          isol = i
+          jsol = j
+        end
+        done = false
+      end
+    end
+  end
+  i, j = isol, jsol
+  α = (j + 0.5) * norm(q) / abs(q[i])
+  qt = round.(Int, α * q / norm(q), RoundNearestTiesAway)
+  qt[i] = (j + 1) * sign(q[i])
+  return qt, α
+end
---
+++ test/test_blackbox.jl
@@ -0,0 +1,55 @@
+function test_blackbox()
+  @testset "Blackbox" begin
+    @testset "Rosenbrock" begin
+      f(x) = (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
+      nlp = SimpleNLPModel(f, [-1.2; 1.0])
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-12)
+      @test norm(x - ones(2)) < 1e-1
+      @test fx < 1e-3
+    end
+
+    @testset "Bounded quadratic" begin
+      f(x) = (x[1] - 1)^2 + 4 * (x[2] - 2)^2
+      nlp = SimpleNLPModel(f, zeros(2), lvar=zeros(2), uvar=0.5*ones(2))
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-6)
+      @test x ≈ 0.5*ones(2)
+    end
+
+    @testset "Linearly constrained quadratic" begin
+      f(x) = (x[1] - 1)^2 + 4 * (x[2] - 2)^2
+      c(x) = x[1] + x[2] - 1.0
+      nlp = SimpleNLPModel(f, zeros(2), c=c, lcon=[-Inf], ucon=[0.0])
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-12)
+      #@test norm(x - [-0.6; 1.6]) < 1e-6
+      @test fx < f(zeros(2))
+    end
+
+    @testset "Linearly constrained quadratic with bounds" begin
+      f(x) = (x[1] - 1)^2 + 4 * (x[2] - 2)^2
+      c(x) = x[1] + x[2] - 1.0
+      nlp = SimpleNLPModel(f, zeros(2), lvar=zeros(2), ucon=ones(2), c=c,
+                           lcon=[-Inf], ucon=[0.0])
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-12)
+      @test norm(x - [0.0; 1.0]) < 1e-4
+      @test fx < f(zeros(2))
+    end
+
+    @testset "Narrow" begin
+      f(x) = dot(x, x)
+      c(x) = x[2] - x[1]
+      ϵ = 1e-4
+      nlp = SimpleNLPModel(f, ones(2), c=c, lcon=[-ϵ], ucon=[ϵ])
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-12)
+      @test norm(x) < 1e-6
+    end
+
+    @testset "Bounded Rosenbrock" begin
+      f(x) = (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
+      nlp = SimpleNLPModel(f, zeros(2), lvar=zeros(2), uvar=0.5*ones(2))
+      x, fx = blackbox(nlp, verbose=false, max_f=1_000_000, tol=1e-12)
+      @test norm(x - [0.5; 0.25]) < 1e-6
+    end
+  end
+end
+
+test_blackbox()
